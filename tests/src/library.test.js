d5ac8782d8014cccb4d706df1979524364b02b35
The Jared Wilcurt <4629794+TheJaredWilcurt@users.noreply.github.com>
2020-09-26 09:54:55 -0400

    generateLinuxFileData 100% coverage

--- tests/src/library.test.js
+++ tests/src/library.test.js
@@ -13,9 +13,117 @@ describe('library', () => {
   });
 
   describe('generateLinuxFileData', () => {
-    test('Placeholder', () => {
-      expect(typeof(library))
-        .toEqual('object');
+    beforeEach(() => {
+      options = {
+        linux: {
+          ...defaults,
+          customLogger,
+          filePath: '/home/DUMMY/file.ext'
+        }
+      };
+    });
+
+    test('Empty options', () => {
+      expect(library.generateLinuxFileData({}))
+        .toEqual('');
+    });
+
+    test('Empty linux options', () => {
+      options.linux = {};
+
+      expect(library.generateLinuxFileData(options))
+        .toEqual('');
+    });
+
+    test('File path', () => {
+      expect(library.generateLinuxFileData(options))
+        .toEqual([
+          '#!/user/bin/env xdg-open',
+          '[Desktop Entry]',
+          'Version=1.0',
+          'Type=Application',
+          'Terminal=false',
+          'Exec=/home/DUMMY/file.ext',
+          'Name=file'
+        ].join('\n'));
+    });
+
+    test('Terminal', () => {
+      options.linux.terminal = true;
+
+      expect(library.generateLinuxFileData(options))
+        .toEqual([
+          '#!/user/bin/env xdg-open',
+          '[Desktop Entry]',
+          'Version=1.0',
+          'Type=Application',
+          'Terminal=true',
+          'Exec=/home/DUMMY/file.ext',
+          'Name=file'
+        ].join('\n'));
+    });
+
+    test('Type', () => {
+      options.linux.type = 'Directory';
+      options.linux.filePath = '/home/DUMMY'
+
+      expect(library.generateLinuxFileData(options))
+        .toEqual([
+          '#!/user/bin/env xdg-open',
+          '[Desktop Entry]',
+          'Version=1.0',
+          'Type=Directory',
+          'Terminal=false',
+          'Exec=/home/DUMMY',
+          'Name=DUMMY'
+        ].join('\n'));
+    });
+
+    test('Name', () => {
+      options.linux.name = 'Test';
+
+      expect(library.generateLinuxFileData(options))
+        .toEqual([
+          '#!/user/bin/env xdg-open',
+          '[Desktop Entry]',
+          'Version=1.0',
+          'Type=Application',
+          'Terminal=false',
+          'Exec=/home/DUMMY/file.ext',
+          'Name=Test'
+        ].join('\n'));
+    });
+
+    test('Comment', () => {
+      options.linux.comment = 'Test';
+
+      expect(library.generateLinuxFileData(options))
+        .toEqual([
+          '#!/user/bin/env xdg-open',
+          '[Desktop Entry]',
+          'Version=1.0',
+          'Type=Application',
+          'Terminal=false',
+          'Exec=/home/DUMMY/file.ext',
+          'Name=file',
+          'comment=Test'
+        ].join('\n'));
+    });
+
+    test('Icon', () => {
+      options.linux.icon = '/home/DUMMY/icon.png';
+
+      expect(library.generateLinuxFileData(options))
+        .toEqual([
+          '#!/user/bin/env xdg-open',
+          '[Desktop Entry]',
+          'Version=1.0',
+          'Type=Application',
+          'Terminal=false',
+          'Exec=/home/DUMMY/file.ext',
+          'Name=file',
+          'Icon=/home/DUMMY/icon.png'
+        ].join('\n'));
     });
   });
 
