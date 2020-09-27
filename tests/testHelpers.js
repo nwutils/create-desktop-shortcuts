1cd73629bcdedef2061716b4ad2a5a9ef8ebb280
TheJaredWilcurt <4629794+TheJaredWilcurt@users.noreply.github.com>
2020-09-26 22:22:14 -0400

    refactor optionSlasher

--- tests/testHelpers.js
+++ tests/testHelpers.js
@@ -23,38 +23,29 @@ const testHelpers = {
     return str.split('\\').join('/');
   },
   optionsSlasher: function (options) {
+    const operatingSystems = [
+      'windows',
+      'linux',
+      'osx'
+    ];
+    const systemPaths = [
+      'filePath',
+      'outputPath',
+      'icon'
+    ];
+
     if (options) {
-      if (options.windows) {
-        if (options.windows.filePath) {
-          options.windows.filePath = this.slasher(options.windows.filePath);
+      operatingSystems.forEach((operatingSystem) => {
+        if (options[operatingSystem]) {
+          systemPaths.forEach((key) => {
+            if (options[operatingSystem][key]) {
+              options[operatingSystem][key] = this.slasher(options[operatingSystem][key]);
+            }
+          });
         }
-        if (options.windows.outputPath) {
-          options.windows.outputPath = this.slasher(options.windows.outputPath);
-        }
-        if (options.windows.icon) {
-          options.windows.icon = this.slasher(options.windows.icon);
-        }
-      }
-      if (options.linux) {
-        if (options.linux.filePath) {
-          options.linux.filePath = this.slasher(options.linux.filePath);
-        }
-        if (options.linux.outputPath) {
-          options.linux.outputPath = this.slasher(options.linux.outputPath);
-        }
-        if (options.linux.icon) {
-          options.linux.icon = this.slasher(options.linux.icon);
-        }
-      }
-      if (options.osx) {
-        if (options.osx.filePath) {
-          options.osx.filePath = this.slasher(options.osx.filePath);
-        }
-        if (options.osx.outputPath) {
-          options.osx.outputPath = this.slasher(options.osx.outputPath);
-        }
-      }
+      });
     }
+
     return options;
   },
   defaults: {
