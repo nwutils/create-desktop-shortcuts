b5e31812fa0540f34530f73ef125c8b7f734e475
The Jared Wilcurt <4629794+TheJaredWilcurt@users.noreply.github.com>
2021-05-19 06:42:16 -0400

    tag-lines

--- .eslintrc.js
+++ .eslintrc.js
@@ -92,6 +92,20 @@ module.exports = {
     'jsdoc/require-returns-check': 1,
     'jsdoc/require-returns-description': 1,
     'jsdoc/require-returns-type': 1,
+    'jsdoc/tag-lines': [
+      1,
+      'always',
+      {
+        'count': 0,
+        'noEndLines': true,
+        'tags': {
+          'example': {
+            'count': 1,
+            'lines': 'always'
+          }
+        }
+      }
+    ],
     'jsdoc/valid-types': 1
   },
   'settings': {
